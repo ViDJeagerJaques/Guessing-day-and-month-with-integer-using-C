27fa00dc5dc5b39b83c34dc2a0cf0c89721dd743
ViD_Atha <119511123+ViDJeagerJaques@users.noreply.github.com>
2025-09-18 22:35:04 +0700

    Delete test.c

--- test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    for(int i = 0; i < 20; i++) {
-        printf("SAYA JANJI TIDAK NGOCOK LAGI, JIKA SAYA NGOOCOK LAGI MAKA SAYA BERJANJILAGI\n");
-    }
-    return 0;
-}
